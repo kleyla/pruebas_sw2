bb176c62cab3a5d27561fd0e2945fd838723f5c4
kleyla <kleyla01@live.com>
2020-10-04 16:59:52 -0400

    solicitud

--- solicitud.php
+++ solicitud.php
@@ -3,30 +3,29 @@
 function store($datos, $lugar, $persona)
 {
     try {
-        if (validar($datos)) {
-            save($datos);
-            if($lugar)
-            if($lugar == 'nuevo' ){
-                if (validar($lugar)){
-                    save($lugar);
+        if (validar($datos)) {              // 1
+            save($datos);                   // 2
+            if($lugar == 'nuevo' ){         // 3
+                if (validar($lugar)){       //4
+                    save($lugar);           // 5
                     $lugar_id = $lugar->id;
                 }else{
                     throw new Exception('Datos vacios!');
                 }
             }else{
-                $lugar_id = $lugar;
+                $lugar_id = $lugar;         // 6
             }
-            if($persona == 'nuevo'){
-                if(validar($persona)){
-                    save($persona);
+            if($persona == 'nuevo'){        // 7
+                if(validar($persona)){      // 8
+                    save($persona);         // 9
                     $persona_id = $persona->id;
                 }else{
                     throw new Exception('Datos vacios!');
                 }
             }else{
-                $persona_id = $persona->id;
+                $persona_id = $persona->id;  //10
             }
-            save($datos);
+            save($datos);                       // 11
         } else {
             throw new Exception('Datos vacios!');
         }
